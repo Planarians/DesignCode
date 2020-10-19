2c099dd71a182bada400f55621f16e886c027677
“planarians” <lyplanarians@gmail.com>
2020-10-19 20:54:42 +0800

    22.01

--- DesignCode/ContentView.swift
+++ DesignCode/ContentView.swift
@@ -9,13 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            VStack {
+                Text("UI Design")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color("accent"))
+                    .padding(.top)
+                Text("Certificate")
+                    .foregroundColor(Color.white)
+            }
+            Image("Background1")
+        }
+        .background(Color.black)
+        .cornerRadius(10)
     }
 }
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone 12 Pro Max")
     }
 }
